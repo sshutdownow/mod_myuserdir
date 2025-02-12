31b36eb167e46074ebcf4a01805812781ebfb97b
Igor Popov <sshutdownow@users.noreply.github.com>
2025-02-13 00:18:46 +0200

    Update tilde_users.sql

--- tilde_users.sql
+++ tilde_users.sql
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2005 Igor Popov <igorpopov@newmail.ru> 
+* Copyright (c) 2005-2025 Igor Popov <ipopovi@gmail.com> 
 *
 * $Id: tilde_users.sql 38 2008-07-15 07:27:33Z igor_popov $
 *
@@ -15,9 +15,11 @@ CREATE TABLE `tilde_users` (
     `extra_php_config` text,
     PRIMARY KEY  (`username`),
     KEY (`enabled`)
-) ENGINE=MyISAM COMMENT='hosting ~users';
+) COMMENT='hosting ~users';
 
-GRANT SELECT ON `hosting`.`tilde_users` TO 'nonpriv'@'localhost' IDENTIFIED BY 'M3Ga_PaSsVVd';
+CREATE USER 'nonpriv'@'localhost' IDENTIFIED BY 'M3Ga_PaSsVVd';
+GRANT SELECT ON `hosting`.`tilde_users` TO 'nonpriv'@'localhost';
+FLUSH PRIVILEGES;
 
 INSERT INTO `tilde_users` VALUES ('igor', '/var/www/homedirs/igor', 'yes', 'safe_mode=1');
 INSERT INTO `tilde_users` VALUES ('someone', '/var/www/homedirs/someone', 'yes', 'register_globals=1');
